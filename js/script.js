37fb3ce155bae20fa3d02465587389b474605751
bishwas350 <crashmindboy10@gmail.com>
2024-10-22 06:57:21 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,142 @@
+// let number = 1 
+// for (let x = 1; x  <=100; x++) {
+//    console.log(x);
+   
+    
+// }
+
+
+
+// let gender = "male"
+// let age = 20
+// let area = "dhaka"
+// let contry = "bangladesh"
+// let havejob = true
+// let salary = 50000
+
+
+
+
+// if (gender === "male") {
+//    if (age <= 25) {
+//       if (area === "dhaka") {
+//          if (havejob === true) {
+//             if (salary >= 50000) {
+//                console.log("You're selected for tha job");
+               
+//             }
+//             else{
+//                console.log("Sorry the candidate must have a minimum 50k salary.");
+               
+//             }
+//          }
+//          else {
+//             console.log("Sorry, the candidate must have a job.");
+            
+//          }
+//       }
+//       else {
+//          console.log("Sorry the candidate must be from Dhaka.");
+         
+//       }
+//    }
+//    else {
+//       console.log("Sorry the candidate must be over 19 years old");
+      
+//    }
+// }
+
+// else {
+//    console.log("Sorry, the candidate must be male.");
+   
+// }
+
+
+
+
+
+
+
+// let gender = "female"
+// let age = 25 
+// let area = "dinajpur"
+// let contry = "bangladesh"
+// let jobHave = true
+// let salary = 300000
+
+
+
+// if (gender === "female") {
+//    if (age <=25) {
+//       if (area === "dinajpur") {
+//          if (contry === "bangladesh") {
+//             if (jobHave === true) {
+//                if (salary <= 300000) {
+//                   console.log("You're selected");
+                  
+//                }
+//                else {
+//                   console.log("Sorry we can't above 300k salary ");
+                  
+//                }
+//             }
+//             else {
+//                console.log("Sorry we must need a job exprience person.");
+               
+//             }
+//          }
+//          else {
+//             console.log("Sorry we need banladeshi candidate .");
+            
+//          }
+//       }
+//       else{
+//          console.log("Sorry we need dinajpuri candidate .");
+         
+//       }
+//    }
+//    else{
+//       console.log("Sorry, You must be 20 years or above ");
+      
+//    }
+// }
+// else{
+//    console.log("Sorry, we need femele candidate ");
+   
+// }
+
+
+
+// let teammembers = ["biswas", "tamim","elham","shamim", "nafis"]
+//    console.log(teammembers);
+   
+// let group = {
+//    area : "dinajpur",
+//    name: "biswas",
+//    age : 20
+// }
+// console.log(group);
+
+
+
+
+// function name() {
+//    let chal = 165
+//    let dal = 200
+//    let murgi =300
+//    console.log(chal + dal + murgi);
+   
+// }
+
+// name()
+
+
+// let { area, name, age } ={
+//     area:"dinajpur",
+//     name:"biswas",
+//     age:20
+// }
+// console.log(name);
+
+
+
